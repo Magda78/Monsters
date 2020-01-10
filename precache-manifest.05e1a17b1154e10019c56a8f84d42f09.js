6c9ea16eef79fab70fa8ac4ba432c1278062db5a
Magda <magda.78@hotmail.com>
2020-01-09 22:08:31 -0500

    Updates

---
+++ precache-manifest.05e1a17b1154e10019c56a8f84d42f09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d1a8d6fb4ddcef5857f6efeee41131d",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "23f15e362037c35b7bbe",
+    "url": "/Monsters/static/css/main.50ae29df.chunk.css"
+  },
+  {
+    "revision": "a1baf6a229decaae66cf",
+    "url": "/Monsters/static/js/2.ed3dd1a8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters/static/js/2.ed3dd1a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23f15e362037c35b7bbe",
+    "url": "/Monsters/static/js/main.39c4d198.chunk.js"
+  },
+  {
+    "revision": "5f2bfad4bde6d4faf430",
+    "url": "/Monsters/static/js/runtime-main.74a4072d.js"
+  }
+]);
